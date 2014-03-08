9b52541f8cb1d69edbd38524a40bf660b4e55a68
Michael Ludmann <miludmann@gmail.com>
2014-03-08 18:22:48 +0100

    Fix ant build for robot part

--- robot.sh
+++ robot.sh
@@ -1,6 +1,6 @@
 #!/bin/zsh
 # Build and run program for a robot unit
-# Bluetooth must be turned on beforehan# Bluetooth must be turned on beforehand
+# Bluetooth must be turned on beforehand
 
 nxjc Units/RemoteControl.java
 nxj -r -o RemoteControl.nxj Units/RemoteControl -n Daneel 
