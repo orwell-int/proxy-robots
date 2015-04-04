7e6dc6ea7b947cdd818827996b4402e99d14b6ae
Michael Ludmann <miludmann@gmail.com>
2015-04-04 16:02:38 +0200

    Fix messages generation

--- proxy-robots-module/src/main/scripts/generate.sh
+++ proxy-robots-module/src/main/scripts/generate.sh
@@ -1,5 +1,10 @@
 #!/bin/zsh
 # generate java code for the protobuff definition
 
-protoc --java_out=proxy-robots-module/src/main/java/ messages/*.proto
+DIR=$(cd "$(dirname "$0")" ; pwd)
+cd "$DIR/../../../.."
+pwd
+MSG_PATH=$DIR/../../../../../messages
+echo $MSG_PATH
+protoc --java_out=proxy-robots-module/src/main/java/ --proto_path=$MSG_PATH $MSG_PATH/*.proto
 
