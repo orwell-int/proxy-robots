40af695ea12c283426adf8f0503cef65f4841de7
Michael Ludmann <miludmann@gmail.com>
2015-05-10 17:03:18 +0200

    Refactor code in ZmqMessageDecoder to simplify the split of String

--- proxy-robots-module/src/main/java/orwell/proxy/zmq/ZmqMessageDecoder.java
+++ proxy-robots-module/src/main/java/orwell/proxy/zmq/ZmqMessageDecoder.java
@@ -6,7 +6,6 @@ import orwell.proxy.EnumMessageType;
 
 public class ZmqMessageDecoder {
     final static Logger logback = LoggerFactory.getLogger(ZmqMessageDecoder.class);
-    private static final byte SPACE_CODE = 32; // ascii code of SPACE character
     private final String routingId;
     private final String typeString;
     private final EnumMessageType type;
@@ -15,32 +14,17 @@ public class ZmqMessageDecoder {
 
     public ZmqMessageDecoder(byte[] raw_zmq_message) {
         zmqMessageString = new String(raw_zmq_message);
-        int indexType = 0;
-        int indexMessage = 0;
-        int index = 0;
-        for (byte item : raw_zmq_message) {
-            if (0 == indexType) {
-                if (SPACE_CODE == item) {
-                    indexType = index + 1;
-                }
-            } else {
-                if (SPACE_CODE == item) {
-                    indexMessage = index + 1;
-                    break;
-                }
-            }
-            ++index;
-        }
+        final String[] zmqMessageStringArray = zmqMessageString.split(" ");
+
+        // raw_zmq_message format:
         // routingID typeString message
-        //           ^    ^
-        //           |    indexMessage
+        //           ^          ^
+        //           |          indexMessage
         //           indexType
-        int lengthRoutingID = indexType - 1;
-        lengthRoutingID = (lengthRoutingID < 0) ? 0 : lengthRoutingID;
-        int lengthType = indexMessage - indexType - 1;
-        lengthType = (lengthType < 0) ? 0 : lengthType;
-        routingId = new String(raw_zmq_message, 0, lengthRoutingID);
-        typeString = new String(raw_zmq_message, indexType, lengthType);
+
+        routingId = zmqMessageStringArray[0];
+        typeString = zmqMessageStringArray[1];
+        message = zmqMessageStringArray[2].getBytes();
         switch (typeString) {
             case "Registered":
                 type = EnumMessageType.REGISTERED;
@@ -58,14 +42,6 @@ public class ZmqMessageDecoder {
                 type = EnumMessageType.UNKNOWN;
                 logback.warn("Message typeString unknown: " + typeString);
         }
-        int lengthMessage = raw_zmq_message.length - lengthType
-                - lengthRoutingID - 2;
-        lengthMessage = (lengthMessage < 0) ? 0 : lengthMessage;
-        message = new byte[lengthMessage];
-        System.arraycopy(raw_zmq_message, indexMessage, message, 0,
-                message.length);
-
-        System.out.flush();
 
         logback.info("Message received: [RoutingID] " + routingId + " [TYPE]: " + type);
     }
