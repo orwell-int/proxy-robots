1e54c5c5cc3d258b9cee9fde5743cd5ef5d1f23e
Michael Ludmann <miludmann@gmail.com>
2015-05-27 14:42:22 +0200

    Refactor udpBeacon test class

--- proxy-robots-module/src/test/java/orwell/proxy/UdpBeaconTest.java
+++ proxy-robots-module/src/test/java/orwell/proxy/UdpBeaconTest.java
@@ -27,6 +27,14 @@ public class UdpBeaconTest {
     private final static int BROADCAST_PORT = 9080;
     private final static int BROADCAST_TIMEOUT_MS = 5000;
     private final static int RECEIVER_BUFFER_SIZE = 512;
+    private final static int ADDRESS_SIZE = 12;
+    private final static int BAD_BYTE = 0xB0;
+    private final static int CHECK_BYTE_STX = 0xA0;
+    private final static int CHECK_BYTE_SEPARATOR = 0xA1;
+    private final static int CHECK_BYTE_ETX = 0x00;
+    private final static String IP_TEST = "127.0.0.1";
+    private static final String PARTIAL_PUSH_PORT = "tcp://*:9001";
+    private static final String PARTIAL_PUB_PORT = "tcp://*:9000";
 
     @TestSubject
     private UdpBeaconFinder udpBeaconFinder;
@@ -48,15 +56,15 @@ public class UdpBeaconTest {
     }
 
     /**
-     * @return "0xA012tcp://*:90010xA112tcp://*:90000x00" as a byte Array
+     * @return "CHECK_BYTE_STX12tcp://*:9001CHECK_BYTE_SEPARATOR12tcp://*:9000CHECK_BYTE_ETX" as a byte Array
      */
-    private byte[] getTestPacketDataBytes(final int checkByteSTX, final int firstHalfDataSize,
-                                          final String firstHalfString, final int checkByteSeparator,
+    private byte[] getTestPacketDataBytes(final int CHECkByteSTX, final int firstHalfDataSize,
+                                          final String firstHalfString, final int CHECkByteSeparator,
                                           final int secondHalfDataSize, final String secondHalfString,
-                                          final int checkByteETX) {
+                                          final int CHECkByteETX) {
         final int byteMask = 0xFF;
         final byte[] resultBytes = new byte[RECEIVER_BUFFER_SIZE];
-        resultBytes[0] = (byte) (checkByteSTX & byteMask);
+        resultBytes[0] = (byte) (CHECkByteSTX & byteMask);
         resultBytes[1] = (byte) firstHalfDataSize;
 
         int bytePos = 2;
@@ -64,7 +72,7 @@ public class UdpBeaconTest {
             resultBytes[bytePos] = b;
             bytePos++;
         }
-        resultBytes[bytePos] = (byte) (checkByteSeparator & byteMask);
+        resultBytes[bytePos] = (byte) (CHECkByteSeparator & byteMask);
         bytePos++;
         resultBytes[bytePos] = (byte) secondHalfDataSize;
         bytePos++;
@@ -72,7 +80,7 @@ public class UdpBeaconTest {
             resultBytes[bytePos] = b;
             bytePos++;
         }
-        resultBytes[bytePos] = (byte) (checkByteETX & byteMask);
+        resultBytes[bytePos] = (byte) (CHECkByteETX & byteMask);
 
         return resultBytes;
     }
@@ -84,8 +92,8 @@ public class UdpBeaconTest {
                                        @Override
                                        public Object answer() throws Throwable {
                                            final DatagramPacket datagramPacket = (DatagramPacket) getCurrentArguments()[0];
-                                           datagramPacket.setAddress(InetAddress.getByName("127.0.0.1"));
-                                           datagramPacket.setData(getTestPacketDataBytes(0xA0, 12, "tcp://*:9001", 0xA1, 12, "tcp://*:9000", 0x00));
+                                           datagramPacket.setAddress(InetAddress.getByName(IP_TEST));
+                                           datagramPacket.setData(getTestPacketDataBytes(CHECK_BYTE_STX, ADDRESS_SIZE, PARTIAL_PUSH_PORT, CHECK_BYTE_SEPARATOR, ADDRESS_SIZE, PARTIAL_PUB_PORT, CHECK_BYTE_ETX));
                                            return null;
                                        }
                                    }
@@ -98,7 +106,7 @@ public class UdpBeaconTest {
 
         verify(mockedDatagramSocket);
         assertTrue(udpBeaconDecoder.hasReceivedCorrectData());
-        assertEquals("127.0.0.1", udpBeaconDecoder.getServerGameIp());
+        assertEquals(IP_TEST, udpBeaconDecoder.getServerGameIp());
         assertEquals("tcp://127.0.0.1:9001", udpBeaconDecoder.getPullerAddress());
         assertEquals("tcp://127.0.0.1:9000", udpBeaconDecoder.getPublisherAddress());
     }
@@ -110,8 +118,8 @@ public class UdpBeaconTest {
                                        @Override
                                        public Object answer() throws Throwable {
                                            final DatagramPacket datagramPacket = (DatagramPacket) getCurrentArguments()[0];
-                                           datagramPacket.setAddress(InetAddress.getByName("127.0.0.1"));
-                                           datagramPacket.setData(getTestPacketDataBytes(0xB0, 12, "tcp://*:9001", 0xA1, 12, "tcp://*:9000", 0x00));
+                                           datagramPacket.setAddress(InetAddress.getByName(IP_TEST));
+                                           datagramPacket.setData(getTestPacketDataBytes(BAD_BYTE, ADDRESS_SIZE, PARTIAL_PUSH_PORT, CHECK_BYTE_SEPARATOR, ADDRESS_SIZE, PARTIAL_PUB_PORT, CHECK_BYTE_ETX));
                                            return null;
                                        }
                                    }
@@ -124,7 +132,7 @@ public class UdpBeaconTest {
 
         verify(mockedDatagramSocket);
         assertFalse(udpBeaconDecoder.hasReceivedCorrectData());
-        assertEquals("127.0.0.1", udpBeaconDecoder.getServerGameIp());
+        assertEquals(IP_TEST, udpBeaconDecoder.getServerGameIp());
         assertNull(udpBeaconDecoder.getPullerAddress());
         assertNull(udpBeaconDecoder.getPublisherAddress());
     }
@@ -136,8 +144,8 @@ public class UdpBeaconTest {
                                        @Override
                                        public Object answer() throws Throwable {
                                            final DatagramPacket datagramPacket = (DatagramPacket) getCurrentArguments()[0];
-                                           datagramPacket.setAddress(InetAddress.getByName("127.0.0.1"));
-                                           datagramPacket.setData(getTestPacketDataBytes(0xA0, 12, "tcp://*:9001", 0xB1, 12, "tcp://*:9000", 0x00));
+                                           datagramPacket.setAddress(InetAddress.getByName(IP_TEST));
+                                           datagramPacket.setData(getTestPacketDataBytes(CHECK_BYTE_STX, ADDRESS_SIZE, PARTIAL_PUSH_PORT, BAD_BYTE, ADDRESS_SIZE, PARTIAL_PUB_PORT, CHECK_BYTE_ETX));
                                            return null;
                                        }
                                    }
@@ -150,7 +158,7 @@ public class UdpBeaconTest {
 
         verify(mockedDatagramSocket);
         assertFalse(udpBeaconDecoder.hasReceivedCorrectData());
-        assertEquals("127.0.0.1", udpBeaconDecoder.getServerGameIp());
+        assertEquals(IP_TEST, udpBeaconDecoder.getServerGameIp());
         assertNull(udpBeaconDecoder.getPullerAddress());
         assertNull(udpBeaconDecoder.getPublisherAddress());
     }
@@ -162,8 +170,8 @@ public class UdpBeaconTest {
                                        @Override
                                        public Object answer() throws Throwable {
                                            final DatagramPacket datagramPacket = (DatagramPacket) getCurrentArguments()[0];
-                                           datagramPacket.setAddress(InetAddress.getByName("127.0.0.1"));
-                                           datagramPacket.setData(getTestPacketDataBytes(0xA0, 12, "tcp://*:9001", 0xA1, 12, "tcp://*:9000", 0xB0));
+                                           datagramPacket.setAddress(InetAddress.getByName(IP_TEST));
+                                           datagramPacket.setData(getTestPacketDataBytes(CHECK_BYTE_STX, ADDRESS_SIZE, PARTIAL_PUSH_PORT, CHECK_BYTE_SEPARATOR, ADDRESS_SIZE, PARTIAL_PUB_PORT, BAD_BYTE));
                                            return null;
                                        }
                                    }
@@ -176,7 +184,7 @@ public class UdpBeaconTest {
 
         verify(mockedDatagramSocket);
         assertFalse(udpBeaconDecoder.hasReceivedCorrectData());
-        assertEquals("127.0.0.1", udpBeaconDecoder.getServerGameIp());
+        assertEquals(IP_TEST, udpBeaconDecoder.getServerGameIp());
         assertNull(udpBeaconDecoder.getPullerAddress());
         assertNull(udpBeaconDecoder.getPublisherAddress());
     }
@@ -188,8 +196,8 @@ public class UdpBeaconTest {
                                        @Override
                                        public Object answer() throws Throwable {
                                            final DatagramPacket datagramPacket = (DatagramPacket) getCurrentArguments()[0];
-                                           datagramPacket.setAddress(InetAddress.getByName("127.0.0.1"));
-                                           datagramPacket.setData(getTestPacketDataBytes(0xA0, 11, "tcp://*:9001", 0xA1, 12, "tcp://*:9000", 0x00));
+                                           datagramPacket.setAddress(InetAddress.getByName(IP_TEST));
+                                           datagramPacket.setData(getTestPacketDataBytes(CHECK_BYTE_STX, 11, PARTIAL_PUSH_PORT, CHECK_BYTE_SEPARATOR, ADDRESS_SIZE, PARTIAL_PUB_PORT, CHECK_BYTE_ETX));
                                            return null;
                                        }
                                    }
@@ -202,7 +210,7 @@ public class UdpBeaconTest {
 
         verify(mockedDatagramSocket);
         assertFalse(udpBeaconDecoder.hasReceivedCorrectData());
-        assertEquals("127.0.0.1", udpBeaconDecoder.getServerGameIp());
+        assertEquals(IP_TEST, udpBeaconDecoder.getServerGameIp());
         assertNull(udpBeaconDecoder.getPullerAddress());
         assertNull(udpBeaconDecoder.getPublisherAddress());
     }
@@ -214,8 +222,8 @@ public class UdpBeaconTest {
                                        @Override
                                        public Object answer() throws Throwable {
                                            final DatagramPacket datagramPacket = (DatagramPacket) getCurrentArguments()[0];
-                                           datagramPacket.setAddress(InetAddress.getByName("127.0.0.1"));
-                                           datagramPacket.setData(getTestPacketDataBytes(0xA0, 12, "tcp://.:9001", 0xA1, 12, "tcp://*:9000", 0x00));
+                                           datagramPacket.setAddress(InetAddress.getByName(IP_TEST));
+                                           datagramPacket.setData(getTestPacketDataBytes(CHECK_BYTE_STX, ADDRESS_SIZE, "tcp://.:9001", CHECK_BYTE_SEPARATOR, ADDRESS_SIZE, PARTIAL_PUB_PORT, CHECK_BYTE_ETX));
                                            return null;
                                        }
                                    }
@@ -228,7 +236,7 @@ public class UdpBeaconTest {
 
         verify(mockedDatagramSocket);
         assertFalse(udpBeaconDecoder.hasReceivedCorrectData());
-        assertEquals("127.0.0.1", udpBeaconDecoder.getServerGameIp());
+        assertEquals(IP_TEST, udpBeaconDecoder.getServerGameIp());
         assertNull(udpBeaconDecoder.getPullerAddress());
         assertNull(udpBeaconDecoder.getPublisherAddress());
     }
@@ -257,8 +265,8 @@ public class UdpBeaconTest {
                                        @Override
                                        public Object answer() throws Throwable {
                                            final DatagramPacket datagramPacket = (DatagramPacket) getCurrentArguments()[0];
-                                           datagramPacket.setAddress(InetAddress.getByName("127.0.0.1"));
-                                           datagramPacket.setData(getTestPacketDataBytes(0xB0, 12, "tcp://*:9001", 0xA1, 12, "tcp://*:9000", 0x00));
+                                           datagramPacket.setAddress(InetAddress.getByName(IP_TEST));
+                                           datagramPacket.setData(getTestPacketDataBytes(BAD_BYTE, ADDRESS_SIZE, PARTIAL_PUSH_PORT, CHECK_BYTE_SEPARATOR, ADDRESS_SIZE, PARTIAL_PUB_PORT, CHECK_BYTE_ETX));
                                            return null;
                                        }
                                    }
@@ -267,8 +275,8 @@ public class UdpBeaconTest {
                         @Override
                         public Object answer() throws Throwable {
                             final DatagramPacket datagramPacket = (DatagramPacket) getCurrentArguments()[0];
-                            datagramPacket.setAddress(InetAddress.getByName("127.0.0.1"));
-                            datagramPacket.setData(getTestPacketDataBytes(0xA0, 12, "tcp://*:9001", 0xA1, 12, "tcp://*:9000", 0x00));
+                            datagramPacket.setAddress(InetAddress.getByName(IP_TEST));
+                            datagramPacket.setData(getTestPacketDataBytes(CHECK_BYTE_STX, ADDRESS_SIZE, PARTIAL_PUSH_PORT, CHECK_BYTE_SEPARATOR, ADDRESS_SIZE, PARTIAL_PUB_PORT, CHECK_BYTE_ETX));
                             return null;
                         }
                     }
@@ -280,9 +288,9 @@ public class UdpBeaconTest {
         udpBeaconFinder.startBroadcasting();
         logback.info(udpBeaconFinder.toString());
 
-        verify(mockedDatagramSocket); // check that 2 tries where performed
+        verify(mockedDatagramSocket); // Check that 2 tries where performed
         assertTrue(udpBeaconDecoder.hasReceivedCorrectData());
-        assertEquals("127.0.0.1", udpBeaconDecoder.getServerGameIp());
+        assertEquals(IP_TEST, udpBeaconDecoder.getServerGameIp());
         assertEquals("tcp://127.0.0.1:9001", udpBeaconDecoder.getPullerAddress());
         assertEquals("tcp://127.0.0.1:9000", udpBeaconDecoder.getPublisherAddress());
     }
